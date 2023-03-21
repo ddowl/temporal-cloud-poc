def40949a1ff9ff303fb437c2abcf152109cdc83
Drew Dowling <drew.dowling@verkada.com>
2023-03-21 13:15:49 -0700

    Add simple workflows to worker

--- py/worker.py
+++ py/worker.py
@@ -1,6 +1,10 @@
 import asyncio
+from datetime import datetime, timedelta
+import logging
 import sys
 from temporalio.worker import Worker
+from temporalio import workflow
+from temporalio import activity
 
 from activity import expensive_activity, unreliable_activity
 from client import create_temporal_client
@@ -9,7 +13,32 @@ from workflow import DummyWorkflow
 
 namespace, cert_path, key_path, codec_key = sys.argv[1:]
 
-interrupt_event = asyncio.Event()
+
+@workflow.defn(sandboxed=False)
+class ReturnFortyTwo:
+    """Test workflow"""
+
+    @workflow.run
+    async def run(self) -> int:
+        return 42
+
+
+@workflow.defn(sandboxed=False)
+class PrintCurrentDate:
+    """Test workflow"""
+
+    @workflow.run
+    async def run(self) -> datetime:
+        return await workflow.execute_activity(
+            print_current_date, schedule_to_close_timeout=timedelta(seconds=5)
+        )
+
+
+@activity.defn
+async def print_current_date() -> datetime:
+    now = workflow.now()
+    logging.getLogger().info("current time: %s", now)
+    return now
 
 
 async def main():
@@ -20,21 +49,14 @@ async def main():
     worker = Worker(
         client,
         task_queue=TASK_QUEUE_NAME,
-        workflows=[DummyWorkflow],
-        activities=[expensive_activity, unreliable_activity],
+        workflows=[DummyWorkflow, ReturnFortyTwo, PrintCurrentDate],
+        activities=[expensive_activity, unreliable_activity, print_current_date],
     )
-    async with worker:
-        print("Worker started, ctrl+c to exit")
-        await interrupt_event.wait()
-        print("Shutting down worker")
+    await worker.run()
 
 
 # Run async main w/ graceful keyboard interrupt shutdown
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    try:
-        loop.run_until_complete(main())
-    except KeyboardInterrupt:
-        interrupt_event.set()
-        loop.run_until_complete(loop.shutdown_asyncgens())
+    asyncio.run(main())
+
